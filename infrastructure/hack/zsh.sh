cb91bfe2f7e45736416e1dff917da24493400ac4
Arman Mazloumzadeh <armanexplorer@gmail.com>
2024-01-16 08:48:54 +0000

    fix(zsh): fix zsh script + make gsutils faster

--- infrastructure/hack/zsh.sh
+++ infrastructure/hack/zsh.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
+set -e
+
 install_zsh() {
-    sudo apt update
-    sudo apt install -y zsh
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    sudo apt update && sudo apt install -y zsh
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
     git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
     sed -i 's/plugins=(git)/plugins=(git zsh-autosuggestions)/' ~/.zshrc
-    chsh -s "$(which zsh)"
     exec zsh
 }
 
